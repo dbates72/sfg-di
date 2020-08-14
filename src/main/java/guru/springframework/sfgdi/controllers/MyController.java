d4616c1f1f1348f0d135066e7ace86fff4933ed5
David Bates <david_bates@mac.com>
2020-08-14 13:05:33 -0700

    updates

--- src/main/java/guru/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/MyController.java
@@ -2,7 +2,6 @@ package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.stereotype.Controller;
-
 /**
  * Created by jt on 12/26/19.
  */
